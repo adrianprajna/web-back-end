365159082c0977e17c7a619700b0ecad25650947
adrianprajna <65269354+adrianprajna@users.noreply.github.com>
2020-07-20 00:11:21 +0700

    first commit back end

---
+++ graph/schema.resolvers.go
@@ -0,0 +1,247 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"Go_BackEnd/graph/generated"
+	"Go_BackEnd/graph/model"
+	"context"
+	"errors"
+	"fmt"
+)
+
+func (r *mutationResolver) CreateVideo(ctx context.Context, input *model.NewVideo) (*model.Video, error) {
+	var video model.Video
+
+	video = model.Video{
+		UserID:         input.UserID,
+		Title:          input.Title,
+		URL:            input.URL,
+		Description:    input.Description,
+		Category:       input.Category,
+		Location:       input.Location,
+		Views:          input.Views,
+		Day:            input.Day,
+		Month:          input.Month,
+		Year:           input.Year,
+		Thumbnail:      input.Thumbnail,
+		Likes:          input.Likes,
+		Dislikes:       input.Dislikes,
+		AgeRestriction: input.AgeRestriction,
+		Privacy:        input.Privacy,
+		Premium:        input.Premium,
+	}
+
+	_, err := r.DB.Model(&video).Insert()
+
+	if err != nil {
+		return nil, errors.New("failed to inset new video!")
+	}
+
+	return &video, nil
+}
+
+func (r *mutationResolver) UpdateVideo(ctx context.Context, id string, input *model.NewVideo) (*model.Video, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) DeleteVideo(ctx context.Context, id string) (bool, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) CreatePlaylist(ctx context.Context, input *model.NewPlaylist) (*model.Playlist, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) UpdatePlaylist(ctx context.Context, id string, input *model.NewPlaylist) (*model.Playlist, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) DeletePlaylist(ctx context.Context, id string) (bool, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) CreateUser(ctx context.Context, input *model.NewUser) (*model.User, error) {
+	var user model.User
+
+	user = model.User{
+		Name:        input.Name,
+		Email:       input.Email,
+		Premium:     input.Premium,
+		ImgURL:      input.ImgURL,
+		Subscribers: input.Subscribers,
+	}
+
+	_, error := r.DB.Model(&user).Insert()
+
+	if error != nil {
+		return nil, errors.New("failed to insert")
+	}
+
+	return &user, nil
+}
+
+func (r *mutationResolver) CreateComment(ctx context.Context, input *model.NewComment) (*model.Comment, error) {
+	var comment model.Comment
+
+	comment = model.Comment{
+		UserID:      input.UserID,
+		VideoID:     input.VideoID,
+		Likes:       input.Likes,
+		Dislikes:    input.Dislikes,
+		Description: input.Description,
+		Day:         input.Day,
+		Month:       input.Month,
+		Year:        input.Year,
+	}
+
+	_, err := r.DB.Model(&comment).Insert()
+
+	if err != nil {
+		return nil, errors.New("failed to add a new comment!")
+	}
+
+	return &comment, nil
+}
+
+func (r *mutationResolver) UpdateComment(ctx context.Context, id string, input *model.NewComment) (*model.Comment, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) DeleteComment(ctx context.Context, id string) (bool, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) CreateReply(ctx context.Context, input *model.NewReply) (*model.Reply, error) {
+	var reply model.Reply
+
+	reply = model.Reply{
+		CommentID:   input.CommentID,
+		UserID: input.UserID,
+		Likes:       input.Likes,
+		Dislikes:    input.Dislikes,
+		Description: input.Description,
+		Day: input.Day,
+		Month: input.Month,
+		Year: input.Year,
+	}
+
+	_, err := r.DB.Model(&reply).Insert()
+
+	if err != nil {
+		return nil, errors.New("failed to reply!")
+	}
+
+	return &reply, nil
+}
+
+func (r *mutationResolver) UpdateReply(ctx context.Context, id string, input *model.NewReply) (*model.Reply, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) DeleteReply(ctx context.Context, id string) (bool, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Videos(ctx context.Context) ([]*model.Video, error) {
+	var videos []*model.Video
+
+	error := r.DB.Model(&videos).Select()
+
+	if error != nil {
+		return nil, errors.New("failed to get all videos!")
+	}
+
+	return videos, nil
+}
+
+func (r *queryResolver) Comments(ctx context.Context) ([]*model.Comment, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Playlists(ctx context.Context) ([]*model.Playlist, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Users(ctx context.Context) ([]*model.User, error) {
+	var users []*model.User
+
+	error := r.DB.Model(&users).Select()
+
+	if error != nil {
+		return nil, errors.New("failed to get all users")
+	}
+
+	return users, error
+}
+
+func (r *queryResolver) Replies(ctx context.Context, commentID string) ([]*model.Reply, error) {
+	var replies []*model.Reply
+
+	err := r.DB.Model(&replies).Where("comment_id = ?", commentID).Select()
+
+	if err != nil {
+		return nil, errors.New("failed to reply a comment!")
+	}
+
+	return replies, nil
+}
+
+func (r *queryResolver) GetUser(ctx context.Context, id string) (*model.User, error) {
+	var user model.User
+
+	//error := r.DB.Model(&user).Where("id = ", id).First()
+	error := r.DB.Model(&user).Where("id = ?", id).First()
+
+	if error != nil {
+		return nil, errors.New("failed to get this user")
+	}
+
+	return &user, nil
+}
+
+func (r *queryResolver) GetUserByEmail(ctx context.Context, email string) (*model.User, error) {
+	var user model.User
+
+	error := r.DB.Model(&user).Where("email = ?", email).First()
+
+	if error != nil {
+		return nil, errors.New("failed to get this user by email")
+	}
+
+	return &user, nil
+}
+
+func (r *queryResolver) GetVideo(ctx context.Context, id string) (*model.Video, error) {
+	var video model.Video
+
+	err := r.DB.Model(&video).Where("id = ?", id).First()
+
+	if err != nil {
+		return nil, errors.New("failed to get this video")
+	}
+
+	return &video, nil
+}
+
+func (r *queryResolver) GetAllComments(ctx context.Context, videoID int) ([]*model.Comment, error) {
+	var comments []*model.Comment
+
+	error := r.DB.Model(&comments).Where("video_id = ?", videoID).Select()
+
+	if error != nil {
+		return nil, errors.New("failed to get all comments")
+	}
+
+	return comments, nil
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
---
+++ server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"Go_BackEnd/graph"
+	"Go_BackEnd/graph/generated"
+	"github.com/go-pg/pg/v9"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/rs/cors"
+)
+
+const defaultPort = "5000"
+
+func main() {
+
+	router := chi.NewRouter()
+
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:4200"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
+	pgDB := pg.Connect(&pg.Options{
+		Addr:     ":5432",
+		User:     "postgres",
+		Password: "root",
+		Database: "tpa_web",
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{DB: pgDB}}))
+
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
